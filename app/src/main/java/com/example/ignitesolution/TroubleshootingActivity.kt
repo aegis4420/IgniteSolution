4f2e2ffb664cced76e1660ce3837046eee8cd63f
aegis4420 <aegis4420@gmail.com>
2024-08-22 16:55:11 -0400

    Setting up TroubleshootingActivity, but thinking instead of using buttons, we want to use RecycleView to be more dynamic and easier to scale.

--- app/src/main/java/com/example/ignitesolution/TroubleshootingActivity.kt
+++ app/src/main/java/com/example/ignitesolution/TroubleshootingActivity.kt
@@ -1,10 +1,7 @@
 package com.example.ignitesolution
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class TroubleshootingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
